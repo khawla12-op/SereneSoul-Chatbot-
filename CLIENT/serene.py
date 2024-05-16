12d72396730cffe1f4f4b0c39acae74217b924ce
bouslama-hamza <ham.bousa98@gmail.com>
2024-05-16 15:50:58 +0100

    Finish App Demo

--- CLIENT/serene.py
+++ CLIENT/serene.py
@@ -1,6 +1,17 @@
+from llama_index.core.storage.storage_context import StorageContext
+from llama_index.core import load_index_from_storage
+
 class Serene():
     def __init__(self):
         pass
 
     def make_prediction(self, content):
-        return "This is a prediction"
+        response =  self.answerMe(content)
+        return response.response
+    
+    def answerMe(self, content):
+        storage_context = StorageContext.from_defaults(persist_dir = 'SERENESOUL/static/Store')
+        index = load_index_from_storage(storage_context)
+        query_engine = index.as_query_engine()
+        response = query_engine.query(content)
+        return response
